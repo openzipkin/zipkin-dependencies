e59ec9a69e3b7511f11cdc221b399e0e62464365
Yuri Shkuro <ys@uber.com>
2016-01-27 23:56:10 -0500

    Extends Spark job to accept time frame parameters and ignore spans outside of this time frame

--- cassandra/src/main/scala/io/zipkin/dependencies/spark/cassandra/ZipkinDependenciesJob.scala
+++ cassandra/src/main/scala/io/zipkin/dependencies/spark/cassandra/ZipkinDependenciesJob.scala
@@ -13,20 +13,60 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object ZipkinDependenciesJob {
 
+  val keyspace = sys.env.getOrElse("CASSANDRA_KEYSPACE", "zipkin")
+
+  val cassandraProperties = Map(
+    "spark.cassandra.connection.host" -> sys.env.getOrElse("CASSANDRA_HOST", "127.0.0.1"),
+    "spark.cassandra.connection.port" -> sys.env.getOrElse("CASSANDRA_PORT", "9042"),
+    "spark.cassandra.auth.username" -> sys.env.getOrElse("CASSANDRA_USERNAME", ""),
+    "spark.cassandra.auth.password" -> sys.env.getOrElse("CASSANDRA_PASSWORD", "")
+  )
+
+  // local[*] master lets us run & test the job locally without setting a Spark cluster
+  val sparkMaster = sys.env.getOrElse("SPARK_MASTER", "local[*]")
+
+  // By default the job only considers spans with timestamps up to previous midnight
+  val defaultEndTs: Long = Time.now.floor(Duration.fromTimeUnit(1, DAYS)).inMilliseconds
+
+  // By default the job only accounts for spans in the 24hrs prior to previous midnight
+  val defaultLookback: Long = Duration.fromTimeUnit(1, DAYS).inMilliseconds
+
+  def main(args: Array[String]) = {
+    new ZipkinDependenciesJob(sparkMaster, cassandraProperties, keyspace).run()
+  }
+}
+
+case class ZipkinDependenciesJob(sparkMaster: String = ZipkinDependenciesJob.sparkMaster,
+                                 cassandraProperties: Map[String, String] = ZipkinDependenciesJob.cassandraProperties,
+                                 keyspace: String = ZipkinDependenciesJob.keyspace,
+                                 endTs: Long = ZipkinDependenciesJob.defaultEndTs,
+                                 lookback: Long = ZipkinDependenciesJob.defaultLookback) {
+
+  val startTs = endTs - lookback
+  val microsUpper = endTs * 1000
+  val microsLower = startTs * 1000
+
   /**
    * RDD needs the data objects to be serializable, at least at some points. The zipkin.common.Span is not serializable,
    * and has a lot of extra stuff not needed for pure dependency graph job. This class captures only what's needed.
    */
-  case class Span(id: Long, traceId: Long, parentId: Option[Long], serviceName: Option[String], annotations: Seq[String]) {
+  case class Span(id: Long, traceId: Long, parentId: Option[Long], timestamp: Option[Long], serviceName: Option[String], annotations: Seq[String]) {
     def mergeSpan(s: Span): Span = {
-      Span(id, traceId, parentId, (serviceName ++ s.serviceName).headOption, this.annotations ++ s.annotations)
+      val minTimestamp = Seq(timestamp, s.timestamp).flatten.reduceOption(_ min _)
+      Span(id, traceId, parentId, minTimestamp, (serviceName ++ s.serviceName).headOption, this.annotations ++ s.annotations)
     }
 
     /**
      * @return true if Span contains at most one of each core annotation, false otherwise
      */
     def isValid: Boolean = {
-      serviceName.isDefined && !Constants.CoreAnnotations.map { c =>
+      // TODO this has the potential to count the same span twice in runs with adjacent time windows.
+      // Ideally, one of `<=` should be strict `<`. Depends on https://github.com/openzipkin/zipkin/issues/924.
+      val inTimeRange: Boolean = timestamp.exists(t => microsLower <= t && t <= microsUpper)
+      if (!inTimeRange) {
+        println(s"Span id=$id, tid=$traceId, ts=$timestamp is outside of time range ($startTs, $endTs]")
+      }
+      inTimeRange && serviceName.isDefined && !Constants.CoreAnnotations.map { c =>
         annotations.count(_ == c) // how many times this core annotation 'c' is mentioned
       }.exists(_ > 1)
     }
@@ -43,43 +83,21 @@ object ZipkinDependenciesJob {
     }
   }
 
-  object Codecs {
-    import com.twitter.zipkin.thriftscala.{Dependencies => ThriftDependencies}
-
-    def spanCodec = CassandraSpanStoreDefaults.SpanCodec
-
-    val dependenciesCodec = new ScroogeThriftCodec[ThriftDependencies](ThriftDependencies)
-  }
-
   private[this] def rowToSpan(row: CassandraRow): Span = {
     val thriftSpan = Codecs.spanCodec.decode(row.getBytes("span"))
     val span = thriftSpan.toSpan
     val annotations = span.annotations.map(_.value)
-    Span(id = span.id, traceId = span.traceId, parentId = span.parentId, serviceName = span.serviceName, annotations)
+    val timestamp: Option[Long] = span.timestamp orElse span.annotations.map(_.timestamp).sorted.headOption
+    Span(id = span.id, traceId = span.traceId, parentId = span.parentId, timestamp = timestamp, serviceName = span.serviceName, annotations)
   }
 
-  val keyspace = sys.env.getOrElse("CASSANDRA_KEYSPACE", "zipkin")
-  val cassandraProperties = Map(
-    "spark.cassandra.connection.host" -> sys.env.getOrElse("CASSANDRA_HOST", "127.0.0.1"),
-    "spark.cassandra.connection.port" -> sys.env.getOrElse("CASSANDRA_PORT", "9042"),
-    "spark.cassandra.auth.username" -> sys.env.getOrElse("CASSANDRA_USERNAME", ""),
-    "spark.cassandra.auth.password" -> sys.env.getOrElse("CASSANDRA_PASSWORD", "")
-  )
-  // local[*] master lets us run & test the job right in our IDE, meaning we don't explicitly have a spark master set up
-  val sparkMaster = sys.env.getOrElse("SPARK_MASTER", "local[*]")
-
-  def main(args: Array[String]) = run(sparkMaster, cassandraProperties, keyspace)
-
-  // parameterized for testing
-  def run(
-    sparkMaster: String = sparkMaster,
-    cassandraProperties: Map[String, String] = cassandraProperties,
-    keyspace: String = keyspace
-  ) {
+  def run() {
     val conf = new SparkConf(true)
       .setAll(cassandraProperties)
       .setMaster(sparkMaster)
       .setAppName(getClass.getName)
+
+    println(s"Running Dependencies job with startTs=$startTs (${Time.fromMilliseconds(startTs)}) and endTs=$endTs (${Time.fromMilliseconds(endTs)})")
 
     val sc = new SparkContext(conf)
 
@@ -103,27 +121,42 @@ object ZipkinDependenciesJob {
       .map { case (parent, child) => ((parent, child), 1L) } // start the count
       .reduceByKey(_ + _) // add up the counts
 
-    val dependencies: DependenciesInfo = aggregates
-      .map { case ((parent: String, child: String), callCount: Long) =>
-        DependenciesInfo(0, 0, Seq(DependencyLinkInfo(parent = parent, child = child, callCount = callCount)))
+    if (aggregates.isEmpty()) {
+      println(s"NO VALID SPANS FOUND IN THE TIME RANGE")
+    } else {
+      val dependencies: DependenciesInfo = aggregates
+        .map { case ((parent: String, child: String), callCount: Long) =>
+        DependenciesInfo(Seq(DependencyLinkInfo(parent = parent, child = child, callCount = callCount)))
       }
-      .reduce(_ + _) // merge DLs under one Dependencies object, which overrides +
+        .reduce(_ + _) // merge DLs under one Dependencies object, which overrides +
 
-    saveToCassandra(sc, keyspace, dependencies)
+      saveToCassandra(sc, keyspace, dependencies)
 
-    println(s"Dependencies: $dependencies")
+      println(s"Dependencies: $dependencies")
+    }
     sc.stop()
   }
 
   def saveToCassandra(sc: SparkContext, keyspace: String, dependencies: DependenciesInfo): Unit = {
-    val day: Long = Time.now.floor(Duration.fromTimeUnit(1, DAYS)).inMilliseconds
-
-    val thrift = dependencies.toDependencies.toThrift
+    val thrift = dependencies.toDependencies(startTs = startTs, endTs = endTs).toThrift
     val blob: Array[Byte] = Codecs.dependenciesCodec.encode(thrift).array()
 
+    val day = Time.fromMilliseconds(endTs).floor(Duration.fromTimeUnit(1, DAYS)).inMilliseconds
     val output = (day, blob)
 
     sc.parallelize(Seq(output)).saveToCassandra(keyspace, "dependencies", SomeColumns("day" as "_1", "dependencies" as "_2"))
+    println(s"Saved with day=$day (${Time.fromMilliseconds(day)})")
   }
 
+  def floorToDay(ts: Long): Long = {
+    Time.fromMilliseconds(ts).floor(Duration.fromTimeUnit(1, DAYS)).inMilliseconds
+  }
+}
+
+object Codecs {
+  import com.twitter.zipkin.thriftscala.{Dependencies => ThriftDependencies}
+
+  def spanCodec = CassandraSpanStoreDefaults.SpanCodec
+
+  val dependenciesCodec = new ScroogeThriftCodec[ThriftDependencies](ThriftDependencies)
 }
