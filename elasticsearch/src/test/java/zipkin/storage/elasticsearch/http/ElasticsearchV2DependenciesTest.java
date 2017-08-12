c339ea47f27405af78e9235c3d5ff81258747a63
Adrian Cole <acole@pivotal.io>
2017-08-12 08:16:04 +0800

    Updates to zipkin 1.29.3 and fixes tests

--- elasticsearch/src/test/java/zipkin/storage/elasticsearch/http/ElasticsearchV2DependenciesTest.java
+++ elasticsearch/src/test/java/zipkin/storage/elasticsearch/http/ElasticsearchV2DependenciesTest.java
@@ -18,7 +18,7 @@ import org.junit.ClassRule;
 public class ElasticsearchV2DependenciesTest extends ElasticsearchDependenciesTest {
 
   @ClassRule public static LazyElasticsearchHttpStorage storage =
-      new LazyElasticsearchHttpStorage("openzipkin/zipkin-elasticsearch:1.29.2");
+      new LazyElasticsearchHttpStorage("openzipkin/zipkin-elasticsearch:1.29.3");
 
   @Override protected ElasticsearchHttpStorage storage() {
     return storage.get();
