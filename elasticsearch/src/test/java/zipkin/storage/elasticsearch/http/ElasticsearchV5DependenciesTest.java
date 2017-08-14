0de82e42ff8cbb99c09b5fe933bc49333f0aa16f
Adrian Cole <acole@pivotal.io>
2017-08-14 16:30:14 +0800

    Adds ES 6 support
    Before it was disabled due to a bug in ES 6 beta 1

--- elasticsearch/src/test/java/zipkin/storage/elasticsearch/http/ElasticsearchV5DependenciesTest.java
+++ elasticsearch/src/test/java/zipkin/storage/elasticsearch/http/ElasticsearchV5DependenciesTest.java
@@ -18,7 +18,7 @@ import org.junit.ClassRule;
 public class ElasticsearchV5DependenciesTest extends ElasticsearchDependenciesTest {
 
   @ClassRule public static LazyElasticsearchHttpStorage storage =
-      new LazyElasticsearchHttpStorage("openzipkin/zipkin-elasticsearch5:1.29.3");
+      new LazyElasticsearchHttpStorage("openzipkin/zipkin-elasticsearch5:1.31.1");
 
   @Override protected ElasticsearchHttpStorage storage() {
     return storage.get();
