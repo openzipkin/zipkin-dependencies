c6f534b057f24a3d5d6a9ff9b1cb4919d195e9f3
Adrian Cole <acole@pivotal.io>
2017-11-12 18:37:54 +0800

    Updates docker to 2.3.0

--- elasticsearch/src/test/java/zipkin/storage/elasticsearch/http/ElasticsearchV2DependenciesTest.java
+++ elasticsearch/src/test/java/zipkin/storage/elasticsearch/http/ElasticsearchV2DependenciesTest.java
@@ -18,7 +18,7 @@ import org.junit.ClassRule;
 public class ElasticsearchV2DependenciesTest extends ElasticsearchDependenciesTest {
 
   @ClassRule public static LazyElasticsearchHttpStorage storage =
-      new LazyElasticsearchHttpStorage("openzipkin/zipkin-elasticsearch:2.2.2");
+      new LazyElasticsearchHttpStorage("openzipkin/zipkin-elasticsearch:2.3.0");
 
   @Override protected ElasticsearchHttpStorage esStorage() {
     return storage.get();
