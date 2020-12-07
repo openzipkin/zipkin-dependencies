ef735404c72414891b5a5408901a5fe208af35ff
adriancole <adrian.f.cole@gmail.com>
2020-12-07 10:50:27 +0800

    removes old junit

--- elasticsearch/src/test/java/zipkin2/dependencies/elasticsearch/ElasticsearchDependenciesJobTest.java
+++ elasticsearch/src/test/java/zipkin2/dependencies/elasticsearch/ElasticsearchDependenciesJobTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 The OpenZipkin Authors
+ * Copyright 2016-2020 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -13,109 +13,103 @@
  */
 package zipkin2.dependencies.elasticsearch;
 
+import java.io.IOException;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.assertj.core.api.Assertions;
 import org.elasticsearch.hadoop.EsHadoopException;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static okhttp3.mockwebserver.SocketPolicy.DISCONNECT_AT_START;
 import static okhttp3.tls.internal.TlsUtil.localhost;
 import static org.apache.commons.net.util.Base64.encodeBase64String;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ElasticsearchDependenciesJobTest {
-  @Rule public MockWebServer es = new MockWebServer();
+class ElasticsearchDependenciesJobTest {
+  MockWebServer es = new MockWebServer();
 
-  @Test
-  public void buildHttps() {
+  @BeforeEach void start() throws IOException {
+    es.start();
+  }
+
+  @AfterEach void stop() throws IOException {
+    es.close();
+  }
+
+  @Test void buildHttps() {
     ElasticsearchDependenciesJob job =
-        ElasticsearchDependenciesJob.builder().hosts("https://foobar").build();
+      ElasticsearchDependenciesJob.builder().hosts("https://foobar").build();
     assertThat(job.conf.get("es.nodes")).isEqualTo("foobar:443");
     assertThat(job.conf.get("es.net.ssl")).isEqualTo("true");
   }
 
-  @Test
-  public void buildAuth() {
+  @Test void buildAuth() {
     ElasticsearchDependenciesJob job =
-        ElasticsearchDependenciesJob.builder().username("foo").password("bar").build();
+      ElasticsearchDependenciesJob.builder().username("foo").password("bar").build();
     assertThat(job.conf.get("es.net.http.auth.user")).isEqualTo("foo");
     assertThat(job.conf.get("es.net.http.auth.pass")).isEqualTo("bar");
   }
 
-  @Test
-  public void authWorks() throws Exception {
+  @Test void authWorks() throws Exception {
     es.enqueue(new MockResponse()); // let the HEAD request pass, so we can trap the header value
     es.enqueue(new MockResponse().setSocketPolicy(DISCONNECT_AT_START)); // kill the job
-    ElasticsearchDependenciesJob job =
-        ElasticsearchDependenciesJob.builder()
-            .username("foo")
-            .password("bar")
-            .hosts(es.url("").toString())
-            .build();
+    ElasticsearchDependenciesJob job = ElasticsearchDependenciesJob.builder()
+      .username("foo")
+      .password("bar")
+      .hosts(es.url("").toString())
+      .build();
 
-    try {
-      job.run();
-      failBecauseExceptionWasNotThrown(EsHadoopException.class);
-    } catch (EsHadoopException e) {
-      // this is ok as we aren't trying to emulate the whole server
-    }
+    assertThatThrownBy(job::run)
+      .isInstanceOf(EsHadoopException.class);
+
     assertThat(es.takeRequest().getHeader("Authorization"))
-        .isEqualTo("Basic " + encodeBase64String("foo:bar" .getBytes("UTF-8")).trim());
+      .isEqualTo("Basic " + encodeBase64String("foo:bar".getBytes(UTF_8)).trim());
   }
 
-  @Test
-  public void authWorksWithSsl() throws Exception {
+  @Test void authWorksWithSsl() throws Exception {
     es.useHttps(localhost().sslSocketFactory(), false);
 
     es.enqueue(new MockResponse()); // let the HEAD request pass, so we can trap the header value
     es.enqueue(new MockResponse().setSocketPolicy(DISCONNECT_AT_START)); // kill the job
 
-    ElasticsearchDependenciesJob.Builder builder =
-        ElasticsearchDependenciesJob.builder()
-            .username("foo")
-            .password("bar")
-            .hosts(es.url("").toString());
+    ElasticsearchDependenciesJob.Builder builder = ElasticsearchDependenciesJob.builder()
+      .username("foo")
+      .password("bar")
+      .hosts(es.url("").toString());
 
     // temporarily hack-in self-signed until https://github.com/openzipkin/zipkin/issues/1683
     builder.sparkProperties.put("es.net.ssl.cert.allow.self.signed", "true");
 
     ElasticsearchDependenciesJob job = builder.build();
 
-    try {
-      job.run();
-      failBecauseExceptionWasNotThrown(EsHadoopException.class);
-    } catch (EsHadoopException e) {
-      // this is ok as we aren't trying to emulate the whole server
-    }
+    assertThatThrownBy(job::run)
+      .isInstanceOf(EsHadoopException.class);
+
     assertThat(es.takeRequest().getHeader("Authorization"))
-        .isEqualTo("Basic " + encodeBase64String("foo:bar" .getBytes("UTF-8")).trim());
+      .isEqualTo("Basic " + encodeBase64String("foo:bar".getBytes("UTF-8")).trim());
   }
 
-  @Test
-  public void parseHosts_default() {
-    Assertions.assertThat(ElasticsearchDependenciesJob.parseHosts("1.1.1.1")).isEqualTo("1.1.1.1");
+  @Test void parseHosts_default() {
+    assertThat(ElasticsearchDependenciesJob.parseHosts("1.1.1.1")).isEqualTo("1.1.1.1");
   }
 
-  @Test
-  public void parseHosts_commaDelimits() {
-    Assertions.assertThat(ElasticsearchDependenciesJob.parseHosts("1.1.1.1:9200,2.2.2.2:9200")).isEqualTo("1.1.1.1:9200,2.2.2.2:9200");
+  @Test void parseHosts_commaDelimits() {
+    assertThat(ElasticsearchDependenciesJob.parseHosts("1.1.1.1:9200,2.2.2.2:9200")).isEqualTo(
+      "1.1.1.1:9200,2.2.2.2:9200");
   }
 
-  @Test
-  public void parseHosts_http_defaultPort() {
-    Assertions.assertThat(ElasticsearchDependenciesJob.parseHosts("http://1.1.1.1")).isEqualTo("1.1.1.1:80");
+  @Test void parseHosts_http_defaultPort() {
+    assertThat(ElasticsearchDependenciesJob.parseHosts("http://1.1.1.1")).isEqualTo("1.1.1.1:80");
   }
 
-  @Test
-  public void parseHosts_https_defaultPort() {
-    Assertions.assertThat(ElasticsearchDependenciesJob.parseHosts("https://1.1.1.1")).isEqualTo("1.1.1.1:443");
+  @Test void parseHosts_https_defaultPort() {
+    assertThat(ElasticsearchDependenciesJob.parseHosts("https://1.1.1.1")).isEqualTo("1.1.1.1:443");
   }
 
-  @Test
-  public void javaSslOptsRedirected() {
+  @Test void javaSslOptsRedirected() {
     System.setProperty("javax.net.ssl.keyStore", "keystore.jks");
     System.setProperty("javax.net.ssl.keyStorePassword", "superSecret");
     System.setProperty("javax.net.ssl.trustStore", "truststore.jks");
--- main/src/test/java/zipkin2/dependencies/ZipkinDependenciesJobTest.java
+++ main/src/test/java/zipkin2/dependencies/ZipkinDependenciesJobTest.java
@@ -15,29 +15,29 @@ package zipkin2.dependencies;
 
 import java.util.Date;
 import java.util.TimeZone;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ZipkinDependenciesJobTest {
-  @Test
-  public void parseDate() {
+class ZipkinDependenciesJobTest {
+  @Test void parseDate() {
     // Date assertions don't assume UTC
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 
     long date = ZipkinDependenciesJob.parseDay("2013-05-15");
     assertThat(new Date(date))
-        .hasYear(2013)
-        .hasMonth(5)
-        .hasDayOfMonth(15)
-        .hasHourOfDay(0)
-        .hasMinute(0)
-        .hasSecond(0)
-        .hasMillisecond(0);
+      .hasYear(2013)
+      .hasMonth(5)
+      .hasDayOfMonth(15)
+      .hasHourOfDay(0)
+      .hasMinute(0)
+      .hasSecond(0)
+      .hasMillisecond(0);
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void parseDate_malformed() {
-    ZipkinDependenciesJob.parseDay("2013/05/15");
+  @Test void parseDate_malformed() {
+    assertThatThrownBy(() -> ZipkinDependenciesJob.parseDay("2013/05/15"))
+      .isInstanceOf(IllegalArgumentException.class);
   }
 }
