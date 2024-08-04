453894cc20221979a900c009e0f46c05dbf1a025
Adrian Cole <adrian.cole@elastic.co>
2024-08-04 15:15:46 +0800

    Updates to latest versions and off snapshot Elasticsearch
    Signed-off-by: Adrian Cole <adrian.cole@elastic.co>

--- cassandra3/src/test/java/zipkin2/storage/cassandra/CassandraContainer.java
+++ cassandra3/src/test/java/zipkin2/storage/cassandra/CassandraContainer.java
@@ -50,7 +50,7 @@ final class CassandraContainer extends GenericContainer<CassandraContainer> {
   );
 
   CassandraContainer() {
-    super(parse("ghcr.io/openzipkin/zipkin-cassandra:3.4.0"));
+    super(parse("ghcr.io/openzipkin/zipkin-cassandra:3.4.1"));
     addExposedPort(9042);
     waitStrategy = Wait.forHealthcheck();
     withLogConsumer(new Slf4jLogConsumer(LOGGER));
